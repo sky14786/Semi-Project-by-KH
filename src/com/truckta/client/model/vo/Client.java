251afb49663d78f5d0620ec401c4c1527637b74b
sky14786 <39879299+sky14786@users.noreply.github.com>
2019-09-16 13:05:23 +0900

    project upload

--- src/com/truckta/client/model/vo/Client.java
+++ src/com/truckta/client/model/vo/Client.java
@@ -6,130 +6,6 @@ public class Client {
 	private String id;
 	private String pw;
 	private String name;
-	private String profile;
-	private Date regDate;
-	private Date modDate;
-	private int userType;
-	private int status;
-	private int reportCount;
-	private String email;
-
-	public Client() {
-	}
-
-	public Client(String id, String pw, String name, String profile, Date regDate, Date modDate, int userType,
-			int status, int reportCount, String email) {
-		super();
-		this.id = id;
-		this.pw = pw;
-		this.name = name;
-		this.profile = profile;
-		this.regDate = regDate;
-		this.modDate = modDate;
-		this.userType = userType;
-		this.status = status;
-		this.reportCount = reportCount;
-		this.email = email;
-	}
-
-	public String getId() {
-		return id;
-	}
-
-	public void setId(String id) {
-		this.id = id;
-	}
-
-	public String getPw() {
-		return pw;
-	}
-
-	public void setPw(String pw) {
-		this.pw = pw;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public String getProfile() {
-		return profile;
-	}
-
-	public void setProfile(String profile) {
-		this.profile = profile;
-	}
-
-	public Date getRegDate() {
-		return regDate;
-	}
-
-	public void setRegDate(Date regDate) {
-		this.regDate = regDate;
-	}
-
-	public Date getModDate() {
-		return modDate;
-	}
-
-	public void setModDate(Date modDate) {
-		this.modDate = modDate;
-	}
-
-	public int getUserType() {
-		return userType;
-	}
-
-	public void setUserType(int userType) {
-		this.userType = userType;
-	}
-
-	public int getStatus() {
-		return status;
-	}
-
-	public void setStatus(int status) {
-		this.status = status;
-	}
-
-	public int getReportCount() {
-		return reportCount;
-	}
-
-	public void setReportCount(int reportCount) {
-		this.reportCount = reportCount;
-	}
-
-	public String getEmail() {
-		return email;
-	}
-
-	public void setEmail(String email) {
-		this.email = email;
-	}
-
-	@Override
-	public String toString() {
-		return "Client [id=" + id + ", pw=" + pw + ", name=" + name + ", profile=" + profile + ", regDate=" + regDate
-				+ ", modDate=" + modDate + ", userType=" + userType + ", status=" + status + ", reportCount="
-				+ reportCount + ", email=" + email + "]";
-	}
-    
-	
-}
-=======
-package com.truckta.client.model.vo;
-
-import java.sql.Date;
-
-public class Client {
-	private String id;
-	private String pw;
-	private String name;
 	private String email;
 	private String profile;
 	private Date regDate;
@@ -155,8 +31,6 @@ public class Client {
 		this.status = status;
 		this.reportCount = reportCount;
 	}
-	
-	
 
 	public Client(String id, String pw, String name, String email) {
 		super();
